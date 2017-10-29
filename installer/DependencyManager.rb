78ac53f7551ee42ecd3856de23c7e7518cf4227e
Aaron Sutton <ajsutton.dev@gmail.com>
2017-10-29 17:54:47 -0400

    Some skeleton code

---
+++ installer/DependencyManager.rb
@@ -0,0 +1,11 @@
+# DependencyManager.rb
+#	Classes for installing frameworks required by simple-obfs
+#
+# Developed by Aaron Sutton
+
+class DependencyManager
+	def self.install
+		dependencies = Array[]
+	end
+end
+
