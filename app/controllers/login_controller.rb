80dc02f2970defeeb757aca4b8c38227af282741
Unknown <lukecrum20@gmail.com>
2018-02-10 13:19:54 -0500

    login

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,6 +3,16 @@ class LoginController < ApplicationController
     data = JSON.parse(request.body.read)
     username = data['username']
     password = data['password']
-    
+    user = User.find_by(username: username)
+    if password == user.password
+      data = {
+        'password' => "password",
+        'port' => "100"
+      }
+      render json: data
+    end
+    if password != user.password
+      head :forbidden
+    end
   end
 end
