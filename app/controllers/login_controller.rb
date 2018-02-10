69c393e99d839eb374011d8b025e3cca43d502f3
Unknown <lukecrum20@gmail.com>
2018-02-10 13:22:36 -0500

    check username

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,15 +4,20 @@ class LoginController < ApplicationController
     username = data['username']
     password = data['password']
     user = User.find_by(username: username)
-    if password == user.password
-      data = {
-        'password' => "password",
-        'port' => "100"
-      }
-      render json: data
+    if user != nil
+      if password == user.password
+        data = {
+          'password' => "password",
+          'port' => "100"
+        }
+        render json: data
+      end
+      if password != user.password
+        head :forbidden
+      end
     end
-    if password != user.password
-      head :forbidden
-    end
+  end
+  if user == nil
+    head :not_found
   end
 end
