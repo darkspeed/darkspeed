40ecef0a7b115bbf26306807964bd5235e15cb38
Unknown <lukecrum20@gmail.com>
2018-02-11 12:06:59 -0500

    rails mailer

---
+++ app/mailers/mailer_mailer.rb
@@ -0,0 +1,2 @@
+class MailerMailer < ApplicationMailer
+end
---
+++ test/mailers/mailer_mailer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MailerMailerTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
