80c119bb10d64bc109de2e1b23e46273e61e92b0
Aaron Sutton <aaronjsutton@icloud.com>
2018-02-11 12:15:59 -0500

    Fix the mailer name

--- test/mailers/previews/mailer_mailer_preview.rb
+++
@@ -1,4 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/mailer_mailer
-class MailerMailerPreview < ActionMailer::Preview
-
-end
--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class MailerMailerTest < ActionMailer::TestCase
+class UserMailerTest < ActionMailer::TestCase
   # test "the truth" do
   #   assert true
   # end
