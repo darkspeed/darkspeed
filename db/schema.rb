f18f13d85b65c8538ee4ed7048b92cd76c863061
Aaron Sutton <aaronjsutton@icloud.com>
2018-02-12 20:07:35 -0500

    Add salt column to user database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180211170728) do
+ActiveRecord::Schema.define(version: 20180213010607) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "username"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180211170728) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.string "salt"
   end
 
 end
