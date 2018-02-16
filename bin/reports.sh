2b527ccebc2bcca02dac9bd583da33dad7e7925c
Aaron Sutton <aaronjsutton@icloud.com>
2018-02-15 23:26:00 -0500

    Fix rsync

--- bin/reports.sh
+++ bin/reports.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-rsync -z --delete-after --quiet /tmp/report.html darkspeed@aaronjsutton.com:/var/www/darkspeed-dev/security/
-rsync -z --delete-after --quiet /tmp/report.html darkspeed@aaronjsutton.com:/var/www/darkspeed-dev/reports/
+rsync -rz --delete-after --quiet /tmp/report.html darkspeed@aaronjsutton.com:/var/www/darkspeed-dev/security/
+rsync -rz --delete-after --quiet /tmp/report.html darkspeed@aaronjsutton.com:/var/www/darkspeed-dev/reports/
