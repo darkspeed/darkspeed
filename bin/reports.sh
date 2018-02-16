11142b340460494222a61b2e8bb8ce2181e4a38d
Aaron Sutton <aaronjsutton@icloud.com>
2018-02-15 23:54:21 -0500

    Fix RuboCop report

--- bin/reports.sh
+++ bin/reports.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rsync -rz --delete-after --quiet /tmp/report.html darkspeed@aaronjsutton.com:/var/www/darkspeed-dev/security/
-rsync -rz --delete-after --quiet /tmp/report.html darkspeed@aaronjsutton.com:/var/www/darkspeed-dev/reports/
+rsync -rz --delete-after --quiet /tmp/rubocop.html darkspeed@aaronjsutton.com:/var/www/darkspeed-dev/reports/
