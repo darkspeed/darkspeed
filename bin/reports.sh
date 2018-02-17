afdfef4534ac4576dbbe66c789d8ce15cc5b0008
Aaron Sutton <aaronjsutton@icloud.com>
2018-02-17 13:07:08 -0500

    Add mkdir to script

--- bin/reports.sh
+++ bin/reports.sh
@@ -2,6 +2,7 @@
 HOST=darkspeed@aaronjsutton.com
 ROOT=/var/www/darkspeed-dev
 
+ssh $HOST mkdir -p $ROOT/report/coverage
 rsync -qrz --delete-after docs/ darkspeed@aaronjsutton.com:$ROOT/
 rsync -qrz --delete-after /tmp/security.html darkspeed@aaronjsutton.com:$ROOT/report
 rsync -qrz --delete-after /tmp/rubocop.html darkspeed@aaronjsutton.com:$ROOT/report
