359a81e6eafa668eafd83c792be776e26a617620
Aaron Sutton <aaronjsutton@icloud.com>
2018-02-17 12:39:02 -0500

    Refactor script

--- bin/reports.sh
+++ bin/reports.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 HOST=darkspeed@aaronjsutton.com
 ROOT=/var/www/darkspeed-dev/
+REPORTS=$ROOT/report
+COVERAGE=$ROOT/$REPORT/coverage
 
 rsync -qrz --delete-after docs/ darkspeed@aaronjsutton.com:$ROOT
-ssh $HOST mkdir -p $ROOT/report/coverage
-rsync -qrz --delete-after /tmp/security.html darkspeed@aaronjsutton.com:$ROOT/reports/
-rsync -qrz --delete-after /tmp/rubocop.html darkspeed@aaronjsutton.com:$ROOT/reports/
-rsync -qrz --delete-after public/coverage darkspeed@aaronjsutton.com:$ROOT/reports/
+rsync -qrz --delete-after /tmp/security.html darkspeed@aaronjsutton.com:$REPORTS
+rsync -qrz --delete-after /tmp/rubocop.html darkspeed@aaronjsutton.com:$REPORTS
+rsync -qrz --delete-after public/coverage darkspeed@aaronjsutton.com:$COVERAGE
