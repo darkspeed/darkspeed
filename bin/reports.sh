592baba8dc1295294b0fff120c336665e651b8c3
Aaron Sutton <aaronjsutton@icloud.com>
2018-02-17 12:33:28 -0500

    Restructure script for Yard

--- bin/reports.sh
+++ bin/reports.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-rsync -qrz --delete-after /tmp/security.html darkspeed@aaronjsutton.com:/var/www/darkspeed-dev/reports/
-rsync -qrz --delete-after /tmp/rubocop.html darkspeed@aaronjsutton.com:/var/www/darkspeed-dev/reports/
-rsync -qrz --delete-after public/coverage darkspeed@aaronjsutton.com:/var/www/darkspeed-dev/reports/
-rsync -qrz --delete-after docs/ darkspeed@aaronjsutton.com:/var/www/darkspeed-dev/
+HOST=darkspeed@aaronjsutton.com
+ROOT=/var/www/darkspeed-dev/
+
+rsync -qrz --delete-after docs/ darkspeed@aaronjsutton.com:$ROOT
+ssh $HOST mkdir -p $ROOT/report/coverage
+rsync -qrz --delete-after /tmp/security.html darkspeed@aaronjsutton.com:$ROOT/reports/
+rsync -qrz --delete-after /tmp/rubocop.html darkspeed@aaronjsutton.com:$ROOT/reports/
+rsync -qrz --delete-after public/coverage darkspeed@aaronjsutton.com:$ROOT/reports/
