398626ec1c382a72087ed2f07ec5f46ba277a883
Aaron Sutton <aaronjsutton@icloud.com>
2018-02-17 14:12:24 -0500

    Coverage dir fix

--- bin/reports.sh
+++ bin/reports.sh
@@ -2,8 +2,8 @@
 HOST=darkspeed@aaronjsutton.com
 ROOT=/var/www/darkspeed-dev
 
-ssh $HOST "mkdir -p $ROOT/report/coverage"
+ssh $HOST "mkdir -p $ROOT/report/"
 rsync -qrz --delete-after docs/ darkspeed@aaronjsutton.com:$ROOT/
 rsync -qrz --delete-after /tmp/security.html darkspeed@aaronjsutton.com:$ROOT/report/
 rsync -qrz --delete-after /tmp/rubocop.html darkspeed@aaronjsutton.com:$ROOT/report/
-rsync -qrz --delete-after public/coverage darkspeed@aaronjsutton.com:$ROOT/report/coverage
+rsync -qrz --delete-after public/coverage darkspeed@aaronjsutton.com:$ROOT/report/
