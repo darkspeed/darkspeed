419faaecb4e1b798036e128ae635cb975390439f
Aaron Sutton <aaronjsutton@icloud.com>
2018-02-17 14:08:36 -0500

    Let's try this again

--- bin/reports.sh
+++ bin/reports.sh
@@ -4,6 +4,6 @@ ROOT=/var/www/darkspeed-dev
 
 ssh $HOST "mkdir -p $ROOT/report/coverage"
 rsync -qrz --delete-after docs/ darkspeed@aaronjsutton.com:$ROOT/
-rsync -qrz --delete-after /tmp/security.html darkspeed@aaronjsutton.com:$ROOT/report
-rsync -qrz --delete-after /tmp/rubocop.html darkspeed@aaronjsutton.com:$ROOT/report
+rsync -qrz --delete-after /tmp/security.html darkspeed@aaronjsutton.com:$ROOT/report/
+rsync -qrz --delete-after /tmp/rubocop.html darkspeed@aaronjsutton.com:$ROOT/report/
 rsync -qrz --delete-after public/coverage darkspeed@aaronjsutton.com:$ROOT/report/coverage
