17b93743c9047456613c7916308607e6ec077b70
Aaron Sutton <aaronjsutton@icloud.com>
2018-02-19 22:22:26 -0500

    Clean up skeleton

--- app/services/darksocks/darksocks.rb
+++ app/services/darksocks/darksocks.rb
@@ -1,4 +1,14 @@
 # @abstract Controls the running ds-server or ss-server processes.
 class DarkSocks
-  
+  def start(profile); end
+
+  def stop(profile); end
+
+  def running?(profile); end
+
+  private
+
+  def make_config(profile); end
+
+  def read_pid(file); end
 end
