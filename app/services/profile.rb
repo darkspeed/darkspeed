7d073166979fc2febc7e7e4b099bd3bb88c083c7
Aaron Sutton <aaronjsutton@icloud.com>
2018-02-19 21:30:00 -0500

    Fix style in Profile

--- app/services/profile.rb
+++ app/services/profile.rb
@@ -5,13 +5,13 @@
 #   Profile.read :main # => {:password: "password", port: 8796}
 #
 class Profile
-  @@filepath = "#{Rails.root}/config/darksocks.yml"
+  @filepath = "#{Rails.root}/config/darksocks.yml"
   # Read the YAML configuration file.
   #
   # @param profile [Symbol] The profile to read from the configuration file.
   # @return [Hash] A hash containing the port and password of the profile.
   def self.read(profile)
-    config = (Psych.load_file(@@filepath).symbolize_keys)
+    config = Psych.load_file(@filepath).symbolize_keys
     return config[profile].symbolize_keys if config[profile]
     nil
   end
