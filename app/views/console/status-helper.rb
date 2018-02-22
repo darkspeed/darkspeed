febb63cad261c3d7724a370f75682c00ee377f1c
Unknown <lukecrum20@gmail.com>
2018-02-21 22:30:15 -0500

    remove check for now

--- app/views/console/status-helper.rb
+++ app/views/console/status-helper.rb
@@ -1,11 +1,19 @@
-require 'Darksocks'
+include Darksocks
 
-def start_server
-
+def start_server(name)
+  case name
+  when "main"
+    Darksocks::Server.start :main
+  when "gateway"
+    Darksocks::Server.start :gateway
+  end
 end
 
 def stop_server(name)
   case name
-    
+  when "main"
+    Darksocks::Server.shutdown :main
+  when "gateway"
+    Darksocks::Server.shutdown :gateway
   end
 end
