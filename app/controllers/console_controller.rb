f5702efada256cef069dd740f7914f188665913f
Unknown <lukecrum20@gmail.com>
2018-02-21 22:21:53 -0500

    crap ui + add a file

--- app/controllers/console_controller.rb
+++ app/controllers/console_controller.rb
@@ -1,4 +1,5 @@
 class ConsoleController < ApplicationController
   def status
+
   end
 end
---
+++ app/views/console/status-helper.rb
@@ -0,0 +1,11 @@
+require 'Darksocks'
+
+def start_server
+
+end
+
+def stop_server(name)
+  case name
+    
+  end
+end
